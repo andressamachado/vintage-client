b8e98ce975eb8a525cd9ef96f439228e674c4c45
andressamachado <andressa.pamachado@gmail.com>
2023-10-11 19:22:12 -0400

    Add Header and route to sign up page to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import SignUpPage from "./pages/SignUpPage/SignUpPage";
 import "./app.scss";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header />
         <Routes>
-          
+          <Route path="/sign-up" element={<SignUpPage />} />
         </Routes>
       </BrowserRouter>
     </div>
