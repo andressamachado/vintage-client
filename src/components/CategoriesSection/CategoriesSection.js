07ce1bab4af0f7e6ef536e67296bd40212bc27e9
andressamachado <andressa.pamachado@gmail.com>
2023-10-16 21:06:36 -0400

    Add categories sectioon

---
+++ src/components/CategoriesSection/CategoriesSection.js
@@ -0,0 +1,42 @@
+import topsCover from "../../assets/images/tops-category.jpg";
+import bottomsCover from "../../assets/images/jeans-category.jpg";
+import outersCover from "../../assets/images/outer.jpeg";
+import accessoriesCover from "../../assets/images/accessories-demo.jpeg";
+import { Link } from "react-router-dom";
+import "./categories-section.scss";
+
+function Categories() {
+  return (
+    <section className="categories">
+      <div className="categories__title">
+        <h2>Clothing and Accessories</h2>
+      </div>
+      <div className="categories__card">
+        <Link to={`/products?category=tops`}>
+          <img src={topsCover} />
+          <h4>Tops</h4>
+        </Link>
+      </div>
+      <div className="categories__card">
+        <Link to={`/products?category=bottoms`}>
+          <img src={bottomsCover} />
+          <h4>Bottoms</h4>
+        </Link>
+      </div>
+      <div className="categories__card">
+        <Link to={`/products?category=outers`}>
+          <img src={outersCover} />
+          <h4>Outers</h4>
+        </Link>
+      </div>
+      <div className="categories__card">
+        <Link to={`/products?category=accessories`}>
+          <img src={accessoriesCover} />
+          <h4>Accessories</h4>
+        </Link>
+      </div>
+    </section>
+  );
+}
+
+export default Categories;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import SlideDeck from "../../components/SlideDeck/SlideDeck";
 import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
+import Categories from "../../components/CategoriesSection/CategoriesSection";
 import "./home-page.scss";
 
 function HomePage() {
@@ -7,6 +8,7 @@ function HomePage() {
     <main className="home-page">
       <SlideDeck />
       <FeaturedProducts />
+      <Categories />
     </main>
   );
 }
